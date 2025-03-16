142039414bee32a0e5f1646ebd67d9c07e99649a
bhimamalbhage <bhimamalbhage@hotmail.com>
2025-03-15 23:00:01 -0700

    add get auth url

--- src/api.js
+++ src/api.js
@@ -83,7 +83,7 @@ export const getAccessToken = async () => {
     const searchParams = new URLSearchParams(window.location.search);
     const code = await searchParams.get("code");
     if (!code) {
-      const response = await fetch("YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT");
+      const response = await fetch("https://6k5apeso4j.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url");
       const result = await response.json();
       const { authUrl } = result;
       return (window.location.href = authUrl);
