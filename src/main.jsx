e0e80157b5cbac0aca7c6fc9962eb643ef2377cf
bhimamalbhage <bhimamalbhage@hotmail.com>
2025-03-23 21:56:32 -0700

    PWA setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 atatus.config('76cc01148fb64638a324f8ffe7163b35').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,3 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+serviceWorkerRegistration.register();
